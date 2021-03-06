a684f75da6f16f4c2a040b72d352f9aa5baeebe2
Marc Hanna <Marc.Hanna-1@ou.edu>
2021-03-06 23:33:17 +0000

    created main function

---
+++ project0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#DEFINE MAX_BUF = 1024
+
+int main(int argc, char **argv){
+	char buf[MAX_BUF];
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		errExit("can't open %s", argv[1]);
+
+	while (charRead = read(fd, buffer, MAX_BUF) > 0)
+		
+
+
+
+
+
+
+
+
+}

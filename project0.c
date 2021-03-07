b30b74530aa2364277704692efad11f5c9439ea7
Mhanna112-code <60158039+Mhanna112-code@users.noreply.github.com>
2021-03-07 03:48:25 -0600

    Update project0.c

--- project0.c
+++ project0.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
-#DEFINE MAX_BUF = 1024
+#define MAX_BUF 1024
+#include<fcntl.h>
+#include<errno.h>
+
 
 int main(int argc, char **argv){
-	char buf[MAX_BUF];
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		errExit("can't open %s", argv[1]);
+        char buffer[MAX_BUF];
+        int fd;
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+                printf("can't open file");
 
-	while (charRead = read(fd, buffer, MAX_BUF) > 0)
-		
+//      while (charRead = read(fd, buffer, MAX_BUF) > 0)
+
+        char test[3] = {'ݐ'};
+        printf("orgchar = %d",test[0]);
+        printf("char = %d",  test[0]|128);
+        if (0x00 < test[0] <= 0x7f){
+                test[0] = test[0] &  127;
+                printf("%02x",test[0]);
+        }
+//      printf("%02x%02x", test[0] & 255, (test[0]/256)&255); 
+        if (0x80 < test[0] < 0x7FF){
+                test[0] = ((test[0] & 255)/256)&255;
+                test[0] = test[0] & 49280;
+                printf("%04x",test[0]);
+        }
+        if (0x800 < test[0] < 0xFFFF){
+                test[0] = ((test[0] & 65535)/65536)&65535;
+                test[0] = test[0] & 14712960;
+                printf("%06x", test[0]);
+        }
+        if (0x10000 < test[0] < 0x1FFFFF){
+//              test[0] = ((test[0] & )/256)&255
+                test[0] = test[0] & 4034953344;
+                printf("%08x", test[0]);
+        }
+
+        return 0;
+//      if (0x80 < test[0] <= 0x7FF)
+//              test[^Z
+}
 
 
 

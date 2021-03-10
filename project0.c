549587f6bd286974603c5e8dc7e5c0b4ded51b8d
Marc Hanna <Marc.Hanna-1@ou.edu>
2021-03-10 07:38:29 +0000

    finished first codepoint algo

--- project0.c
+++ project0.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv){
 //	fd = open(argv[1], O_RDONLY);
 //	if (fd == -1)
 //		while (charRead = fread(fd, buffer, MAX_BUF) > 0);		
-	char test[] = "؈";
+	char test[] = "/";
 	int len = strlen(test);
 	char mask[(len*2) + 1];
 	int i=0;
@@ -24,16 +24,16 @@ int main(int argc, char **argv){
 	mask[i++] = '\0';
 	printf("hex_str: %s\n", mask);
 	int i_mask = strtol(mask, NULL, 16);
-	printf("%d", i_mask);
-	return 0;
-	}
+//	printf("%d", i_mask);
+//	uint8_t bytes[4];
+
 //	printf("char = %d",  test[0]|128);
-/*	if (0x00 <(int) test<= 0x7f){
-		test[0] = test &  127;
-		printf("%02x",test[0]);
+	if (0x00 <i_mask<= 0x7f){
+		i_mask = i_mask &  127;
+		printf("%02x",i_mask);
 	}
 //	printf("%02x%02x", test[0] & 255, (test[0]/256)&255); 
-	if (0x80 <(int) test < 0x7FF){
+/*	if (0x80 <(int) test < 0x7FF){
 		mask = 24704;
 		for (int i = 0; i < 6; i++)
 			mask  = mask | (test & (i + 1));
@@ -51,9 +51,9 @@ int main(int argc, char **argv){
 		test[0] = test[0] & 4034953344;
 		printf("%08x", test[0]);
 	}
-
+	//      if (0x80 < test[0] <= 0x7FF)
+//              test[^Z
+*/
 	return 0;
 }
-//	if (0x80 < test[0] <= 0x7FF)
-//		test[
-		*/
+	

35ccf1a7c2ba76397108c9407af7e098a2be7743
Marc Hanna <Marc.Hanna-1@ou.edu>
2021-03-11 11:29:25 +0000

    finished all encodings

--- newproject0.c
+++ newproject0.c
@@ -11,10 +11,10 @@
 #define size4 1048576
 struct element{
 	int count;
-	char byte1;
-	char byte2;
-	char byte3;
-	char byte4;
+	unsigned char byte1;
+	unsigned char byte2;
+	unsigned char byte3;
+	unsigned char byte4;
 	bool isVal;
 };
 
@@ -39,10 +39,12 @@ int main(int argc, char *argv){
     ch = fgetc(fd);
     while (ch  != EOF)
         {
+	if (ch == 0X0A)
+		break;
     /*        printf("%c", ch);
 	    printf("====");
     */
-    printf("%08X", (unsigned char)ch);
+   // printf("%08X", (unsigned char)ch);
     printf("===");
     if ((unsigned char)ch < 128)
 	result = 1;
@@ -59,6 +61,15 @@ int main(int argc, char *argv){
     int b;
     int c;
     int d;
+    char enc2[2];
+    char enc3[3];
+    char enc4[4];
+    memset(enc2, 0, sizeof(enc2));
+    memset(enc3, 0, sizeof(enc3));
+    memset(enc4, 0, sizeof(enc4));
+    char twoC;
+    char threeC;
+    char fourC;
     if (result ==  1){
 	for (a = 0; a < size1; a++){
 		if (ASCIInums[a].isVal == 0)
@@ -76,69 +87,85 @@ int main(int argc, char *argv){
 	printf("%c", ASCIInums[i++].byte1);
 	}
     }
-    ch = fgetc(fd);
-    /*
-	com = 1;
+
     if (result == 2){
+	twoC = fgetc(fd);
         for (b = 0; b < size2; b++){
                 if (UTFG1[b].isVal == 0) 
                         continue;
-                com = strcmp(UTFG1[b].hexString, en);
-                if (com == 0)
+                com = (UTFG1[b].byte1 == ch)&(UTFG1[b].byte2 == twoC);
+                if (com == 1)
                    break;
         }
-        if (com==0)
+        if (com==1)
                 UTFG1[b].count++;
         else{
         UTFG1[j].isVal = 1;
         UTFG1[j].count++;
-        UTFG1[j].hexString = malloc(1 + result);
-        strcpy(UTFG1[j++].hexString, en);
+        UTFG1[j].byte1 = (unsigned char) ch;
+	UTFG1[j].byte2 = (unsigned char) twoC;
+	enc2[0] = UTFG1[j].byte1;
+	enc2[1] = UTFG1[j].byte2;
+	printf("%c", enc2);
+	j++;
         }
     }
-    com = 1;
+
     if (result == 3){
-        for (c = 0; c < size3; a++){
+	twoC = fgetc(fd);
+	threeC = fgetc(fd);
+        for (c = 0; c < size3; c++){
                 if (UTFG2[c].isVal == 0)
                         continue;
-                com = strcmp(UTFG2[c].hexString, en);
-                if (com == 0)
+                com = (UTFG2[c].byte1 == ch) & (UTFG2[c].byte2 == twoC) & (UTFG2[c].byte3 == threeC);
+                if (com == 1)
                    break;
         }
-        if (com==0)
+        if (com==1)
                 UTFG2[c].count++;
         else{
         UTFG2[k].isVal = 1;
         UTFG2[k].count++;
-        UTFG2[k].hexString = malloc(1 + result);
-        strcpy(UTFG2[k++].hexString, en);
-        }
+        UTFG2[k].byte1 = (unsigned char)ch;
+	UTFG2[k].byte2 = (unsigned char)twoC;
+	UTFG2[k].byte3 = (unsigned char)threeC;
+        enc3[0] = UTFG2[k].byte1;
+ 	enc3[1] = UTFG2[k].byte2;
+	enc3[2] = UTFG2[k].byte3;
+        printf("%s", enc3);
+	k++;
+       	}
     }
-    com = 1;
-    printf("%d", result);
+    
     if (result == 4){
+	twoC = fgetc(fd);
+        threeC = fgetc(fd);
+	fourC = fgetc(fd);
         for (d = 0; d < size4; d++){
                 if (UTFG3[d].isVal == 0)
                         continue;
-                com = strcmp(UTFG3[d].hexString, en);
-                if (com == 0)
+                com = (UTFG3[d].byte1 == ch)&(UTFG3[d].byte2 == twoC)&(UTFG3[d].byte3 == threeC)&(UTFG3[d].byte4 == fourC);
+                if (com == 1)
                    break;
         }
-        if (com==0)
+        if (com==1)
                 UTFG3[d].count++;
         else{
         UTFG3[f].isVal = 1;
         UTFG3[f].count++;
-        UTFG3[f].hexString = malloc(1 + result);
-        strcpy(UTFG3[f++].hexString, en);
-        }
-	    printf("%s", UTFG3[0].hexString);
-	    printf("%d", UTFG3[0].count);
-	    printf("%d", UTFG3[0].isVal);
-	    printf("||");
-    	}
+        UTFG3[f].byte1 = (unsigned char)ch;
+        UTFG3[f].byte2 = (unsigned char)twoC;
+	UTFG3[f].byte3 = (unsigned char)threeC;
+	UTFG3[f].byte4 = (unsigned char)fourC;
+	enc4[0] = UTFG3[f].byte1;
+        enc4[1] = UTFG3[f].byte2;
+	enc4[2] = UTFG3[f].byte3;
+	enc4[3] = UTFG3[f].byte4;
+	printf("%s", enc4);
+        k++;
+       	}
     }
-    */
+    ch = fgetc(fd);
 	}
     return 0;
 }

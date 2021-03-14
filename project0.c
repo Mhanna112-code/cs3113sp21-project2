a22866e83d5a387aeb26b45b308e61ef2ec6bc42
Marc Hanna <Marc.Hanna-1@ou.edu>
2021-03-14 04:07:25 +0000

    printg unsigned char

--- project0.c
+++ project0.c
@@ -45,12 +45,12 @@ void printOut(struct element elements[], int structSize){
         }
     }
     for (i  = 0; i < structSize; i++){
-        char arr[4];
+        unsigned char arr[4];
         arr[0] = elements[i].byte1;
         arr[1] = elements[i].byte2;
         arr[2] = elements[i].byte3;
         arr[3] = elements[i].byte4;
-        char *str = &arr[0];
+        unsigned char *str = &arr[0];
         printf("%s", str);
         printf("->");
         printf("%d\n", elements[i].count);
